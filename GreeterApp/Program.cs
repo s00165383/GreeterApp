c1de468f4e189147abf10425c8b29fbba601a4f7
kateoneill <S00165383@mail.itsligo.ie>
2018-09-17 17:48:43 +0100

    WIP

--- GreeterApp/Program.cs
+++ GreeterApp/Program.cs
@@ -9,14 +9,17 @@ namespace GreeterApp
         {
             //Greeter is a terminal application that greets old friends warmly,
             //and remembers new friends.
-            FileStream fs = new FileStream("friends.txt", FileMode.Open, FileAccess.Read);
 
-            StreamReader inputStream = new StreamReader(fs);
+                string lineIn;  // variable where data from file is stored
+                string[] fields = new string[3];
+                FileStream fs;
 
-            string lineIn;  // variable where data from file is stored
-            string[] fields = new string[3];
+                //Reading from file
+                fs = new FileStream("friends.txt", FileMode.Open, FileAccess.Read);
+                StreamReader inputStream = new StreamReader(fs);
+                lineIn = inputStream.ReadLine();
+         
 
-            lineIn = inputStream.ReadLine();
 
 
             //while (lineIn != null)
@@ -32,9 +35,10 @@ namespace GreeterApp
             Console.WriteLine("\t**********************************************");
             Console.WriteLine("\nWelcome to Library.");
             Console.WriteLine("What do you want to do?");
-            Console.WriteLine("1. Enter a name.");
-            Console.WriteLine("2. See a list of all users.");
-            Console.WriteLine("3. Quit.");
+            Console.WriteLine("1. Check if a friend exists.");
+            Console.WriteLine("2. See a list of all friends.");
+            Console.WriteLine("3. Add a friend.");
+            Console.WriteLine("4. Quit.\n");
             string userChoice = Console.ReadLine();
             switch (userChoice)
             {
@@ -49,14 +53,15 @@ namespace GreeterApp
                         if (fields[0] == selectedName)
                         {
                             exists = true;
-                            break;
+                            if (exists)
+                                Console.WriteLine("Welcome back {0}", fields[0]);
+                            else
+                                Console.WriteLine("A new friend, yayy");
+
                         }
                         lineIn = inputStream.ReadLine();
                     }
-                    if (exists)
-                        Console.WriteLine("Welcome back {0}", fields[0]);
-                    else
-                        Console.WriteLine("A new user, yayy");
+
                     break;
                 case "2":
                     while (lineIn != null)
@@ -64,9 +69,25 @@ namespace GreeterApp
                         fields = lineIn.Split(',');
                         Console.WriteLine(fields[0]);
                         lineIn = inputStream.ReadLine();
+                        
                     }
                     break;
                 case "3":
+                    Console.WriteLine("Enter new friends name");
+                    string newName = Console.ReadLine();
+                    Console.WriteLine("Enter where friend lives");
+                    string newLocation = Console.ReadLine();
+                    Console.WriteLine("Enter new friends number");
+                    string newNumber = Console.ReadLine();
+                    inputStream.Close();
+                    //Writing to file
+                    FileStream wfs = new FileStream("friends.txt", FileMode.Append, FileAccess.Write);
+                    StreamWriter outputStream = new StreamWriter(wfs);
+                    outputStream.WriteLine("{0},{1},{2}", newName, newLocation, newNumber);
+                    outputStream.Close();
+                    wfs.Close();
+                    break;
+                case "4":
                     return;
                 default:
                     Console.WriteLine("Unknown selection");
