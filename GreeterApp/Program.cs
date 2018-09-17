3f425b64f1ea267e96ff490f1df07e2ae7db31f0
kateoneill <S00165383@mail.itsligo.ie>
2018-09-17 14:39:09 +0100

    Initial Commit

--- GreeterApp/Program.cs
+++ GreeterApp/Program.cs
@@ -6,7 +6,7 @@ namespace GreeterApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World test!");
         }
     }
 }
